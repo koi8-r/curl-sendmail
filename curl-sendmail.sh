1416e43de19e407367e587f49704be605dc412c5
Валентин Бурченя <vp.burchenya@kolskiy.lan.erc51.info>
2015-01-15 14:21:19 +0300

    Extra quotes around from and recipient params in curl command

--- curl-sendmail.sh
+++ curl-sendmail.sh
@@ -63,7 +63,7 @@ EOF
 ) \
     | sed s/$/$'\r'/ \
     | debug \
-    | curl -s --ssl-reqd -u "$GMAIL_LOGIN:$GMAIL_PASSWD" smtps://smtp.gmail.com:465 --mail-from "'$FROM'" --mail-rcpt "'$TO'" -T - </dev/null >/dev/null 2>&1
+    | curl -s --ssl-reqd -u "$GMAIL_LOGIN:$GMAIL_PASSWD" smtps://smtp.gmail.com:465 --mail-from "$FROM" --mail-rcpt "$TO" -T - </dev/null >/dev/null 2>&1
 
 ERROR=$?
 
